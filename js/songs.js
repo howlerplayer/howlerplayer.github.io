e2dcbcb40bda04474b921139f4bf51b10b078e7d
Irina <irbris@mail.ru>
2019-03-17 08:09:38 +0200

    Add files via upload

--- js/songs.js
+++ js/songs.js
@@ -2,51 +2,51 @@ function CreatePlayer() {
   var playListSongs = [
     {
       artist: 'Сваты',
-      title: 'В городе весна',
-      file: 'http://music42.my.mail.ru/file/979eff98e68d058d29073d5381a71b1a.mp3',
-      duration: '1:10',
+      title: 'Вот и стали дни короче',
+      file: 'https://raw.githubusercontent.com/howlerplayer/howlerplayer.github.io/master/Песня%20к%20сериалу%20Сваты%20-%20Вот%20и%20стали%20дни%20короче.mp3',
+      duration: '2:17',
       howl: null
     },
     {
       artist: 'Сваты',
-      title: 'Сны',
-      file: 'http://music36.my.mail.ru/file/400ea8beb2d417e0fac3c358dc094ea0.mp3',
-      duration: '2:29',
+      title: 'Всё повторится',
+      file: 'https://raw.githubusercontent.com/howlerplayer/howlerplayer.github.io/master/Песня%20к%20сериалу%20Сваты%20-%20Всё%20повторится.mp3',
+      duration: '2:05',
       howl: null
     },
     {
       artist: 'Сваты',
-      title: 'Радостная грусть',
-      file: 'http://music27.my.mail.ru/file/fa4a6e66d828e22f1536ce6570756c75.mp3',
-      duration: '3:28',
+      title: 'Инструментал',
+      file: 'https://raw.githubusercontent.com/howlerplayer/howlerplayer.github.io/master/Песня%20к%20сериалу%20Сваты%20-%20Инструментал.mp3',
+      duration: '3:25',
       howl: null
     },
     {
       artist: 'Сваты',
-      title: 'Прохлады.net',
-      file: 'http://music41.my.mail.ru/file/56ea2d19c1e44d23bf9ff101c4f8158d.mp3',
-      duration: '1:49',
+      title: 'Календарь',
+      file: 'https://raw.githubusercontent.com/howlerplayer/howlerplayer.github.io/master/Песня%20к%20сериалу%20Сваты%20-%20Календарь.mp3',
+      duration: '3:29',
       howl: null
     },
     {
       artist: 'Сваты',
-      title: 'Где же ты был',
-      file: 'http://music41.my.mail.ru/file/51ed5a581433351ca6866d908ad74558.mp3',
-      duration: '2:10',
+      title: 'Лето',
+      file: 'https://raw.githubusercontent.com/howlerplayer/howlerplayer.github.io/master/Песня%20к%20сериалу%20Сваты%20-%20Лето.mp3',
+      duration: '1:25',
       howl: null
     },
     {
       artist: 'Сваты',
-      title: 'Жара',
-      file: 'http://music25.my.mail.ru/file/919dde4beda94468644aac4543237948.mp3',
-      duration: '1:27',
+      title: 'Погоня',
+      file: 'https://raw.githubusercontent.com/howlerplayer/howlerplayer.github.io/master/Песня%20к%20сериалу%20Сваты%20-%20Погоня.mp3',
+      duration: '2:04',
       howl: null
     },
     {
       artist: 'Сваты',
-      title: 'Занавесочки',
-      file: 'http://music37.my.mail.ru/file/59956d79c3d00e2513f57abe96b8f9e2.mp3',
-      duration: '2:40',
+      title: 'Зарядка',
+      file: 'https://raw.githubusercontent.com/howlerplayer/howlerplayer.github.io/master/Песня%20к%20сериалу%20Сваты%20-%20%20Зарядка.mp3',
+      duration: '2:47',
       howl: null
     }
   ]
